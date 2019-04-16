d7ae5fcb6330e7fff8fed804bcc8d7646f9182da
C3NZ <cenz@cenz.io>
2019-04-16 11:59:08 -0700

    [feat] add correlation plot

--- boston.py
+++ boston.py
@@ -1,10 +1,12 @@
 import argparse
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import seaborn as sns
 from sklearn.datasets import load_boston
-
-# Create a correlation heatmap for determining how highly correletated our
-# predictor values are (the higher, the worse)
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 # Train the model using our training data (75% training, 25% testing)
 
@@ -19,6 +21,7 @@ def add_arguments(parser):
         Add arguments to the arg parser
     """
     parser.add_argument("-d, --data", dest="dataframe", action="store_true")
+    parser.add_argument("-c, --correlation", dest="correlation", action="store_true")
     parser.print_help
 
 
@@ -30,7 +33,51 @@ def create_dataframe():
     bos_df = pd.DataFrame(boston.data)
     bos_df.columns = boston.feature_names
     bos_df["PRICE"] = boston.target
+    return bos_df
 
+
+def get_model_data(dataframe):
+    """
+        Get the training and testing data
+    """
+    print("Creating model data")
+    X_data = []
+    y_data = []
+
+    for row in dataframe.values:
+        X_data.append(row[:-1])
+        y_data.append(row[-1])
+
+    X_data = np.array(X_data)
+    y_data = np.array(y_data)
+
+    return train_test_split(X_data, y_data)
+
+
+def create_model(X_data, y_data):
+    """
+        Linear regression data
+    """
+    print("Creating linear regression model")
+    linear_reg = LinearRegression()
+    linear_reg.fit(X_data, y_data)
+    return linear_reg
+
+
+def get_correlation(dataframe):
+    """
+        Correlate our data
+    """
+
+    X_train, X_test, y_train, y_test = get_model_data(dataframe)
+
+    linear_reg = create_model(X_train, y_train)
+
+    corr = dataframe.corr()
+    sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns)
+    plt.show()
+
+def test_models(1)
 
 def main():
     """
@@ -43,8 +90,10 @@ def main():
     add_arguments(parser)
     # parse the arguments entered by the userhj
     args = parser.parse_args()
-    print(args)
     dataframe = create_dataframe()
+
+    if args.correlation:
+        get_correlation(dataframe)
 
 
 if __name__ == "__main__":
